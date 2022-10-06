3b691072952dc5d5b790cc64d5df42e24f323736
robjrdev <85550412+robjrdev@users.noreply.github.com>
2022-10-06 20:57:32 +0800

    Add gitignore file

--- assets/scripts/greetings.js
+++ assets/scripts/greetings.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 //DOM SELECTOR
 //Time
-const currentTime = document.querySelector(".current-time");
-const greeting = document.querySelector(".greeting");
-const showToggle = document.querySelector(".btn-show--toggle");
-const toggleFormat = document.querySelector(".switch");
-const switchToggle = document.querySelector(".toggle--switch");
+const currentTime = document.querySelector('.current-time');
+const greeting = document.querySelector('.greeting');
+const showToggle = document.querySelector('.btn-show--toggle');
+const toggleFormat = document.querySelector('.switch');
+const switchToggle = document.querySelector('.toggle--switch');
 //User name
-const userName = document.getElementById("user-name");
-const getName = document.getElementById("get-name");
-const inputName = document.querySelector(".input-name");
-const btnSubmit = document.querySelector(".btn--name");
+const userName = document.getElementById('user-name');
+const getName = document.getElementById('get-name');
+const inputName = document.querySelector('.input-name');
+const btnSubmit = document.querySelector('.btn--name');
 
 //Focus
-const focusSection = document.querySelector(".focus");
-const focusInput = document.querySelector(".focus-input");
-const focusBtn = document.querySelector(".btn--focus");
-const focusMessage = document.querySelector(".focus-message");
+const focusSection = document.querySelector('.focus');
+const focusInput = document.querySelector('.focus-input');
+const focusBtn = document.querySelector('.btn--focus');
+const focusMessage = document.querySelector('.focus-message');
 
 //Get time on machine
 let time;
@@ -25,13 +25,13 @@ const generateTime = function () {
   const timeFormat = switchToggle.checked;
 
   if (timeFormat) {
-    time = date.toLocaleTimeString("en-US", {
-      timeStyle: "short",
+    time = date.toLocaleTimeString('en-US', {
+      timeStyle: 'short',
       hour12: false,
     });
   } else {
-    time = date.toLocaleTimeString("en-US", {
-      timeStyle: "short",
+    time = date.toLocaleTimeString('en-US', {
+      timeStyle: 'short',
     });
   }
   return time;
@@ -41,19 +41,19 @@ generateTime();
 currentTime.innerHTML = time;
 
 //Show Toggle
-showToggle.addEventListener("click", function () {
-  toggleFormat.classList.toggle("hidden");
+showToggle.addEventListener('click', function () {
+  toggleFormat.classList.toggle('hidden');
 });
 
 //Costumize Greeting watch time
 const hours = new Date().getHours();
 const costumGreet = function () {
   if (hours < 12) {
-    greeting.textContent = "Good Morning,";
+    greeting.textContent = 'Good Morning,';
   } else if (hours < 18) {
-    greeting.textContent = "Good afternoon,";
+    greeting.textContent = 'Good afternoon,';
   } else {
-    greeting.textContent = "Good evening,";
+    greeting.textContent = 'Good evening,';
   }
 };
 costumGreet();
@@ -62,38 +62,38 @@ costumGreet();
 const pressEnter = function (e) {
   //Get user name
 
-  if (e.key === "Enter") {
+  if (e.key === 'Enter') {
     if (getName.value.length !== 0) {
-      inputName.classList.add("hidden");
+      inputName.classList.add('hidden');
       userName.textContent = getName.value;
-      localStorage.setItem("currentUser", getName.value);
+      localStorage.setItem('currentUser', getName.value);
     }
 
     //Get focus input
     if (focusInput.value.length !== 0) {
       focusMessage.textContent = focusInput.value;
-      focusSection.classList.add("hidden");
-      localStorage.setItem("userFocus", focusInput.value);
+      focusSection.classList.add('hidden');
+      localStorage.setItem('userFocus', focusInput.value);
     }
   }
 };
 
 //get name from user
-inputName.addEventListener("keypress", pressEnter);
+inputName.addEventListener('keypress', pressEnter);
 
 //get focus message
-focusSection.addEventListener("keypress", pressEnter);
+focusSection.addEventListener('keypress', pressEnter);
 
 //Get userInput from localStorage
-const currentUser = localStorage.getItem("currentUser");
+const currentUser = localStorage.getItem('currentUser');
 userName.textContent = currentUser;
-const currentFocus = localStorage.getItem("userFocus");
+const currentFocus = localStorage.getItem('userFocus');
 focusMessage.textContent = currentFocus;
 
 // Validation if user input is needed
 if (userName.textContent.length !== 0) {
-  inputName.classList.add("hidden");
+  inputName.classList.add('hidden');
 }
 if (focusMessage.textContent.length !== 0) {
-  focusSection.classList.add("hidden");
+  focusSection.classList.add('hidden');
 }
